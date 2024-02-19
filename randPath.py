90addfbb35964e548b7ae53aed1aae071e920c6e
Shahab Mac <shokohishahab@gmail.com>
2024-02-18 22:12:53 -0500

    PPO being added

--- randPath.py
+++ randPath.py
@@ -14,11 +14,48 @@ n = 5  # N by N view of the robot, must be odd
 x = np.arange(0, n + 1)
 path_planner = SmartPSB()
 target = np.array([10, 0])
+possible_actions = np.arange(0, 5)
 num_samples = 1000
-grids = GridDataset(n, num_samples)
+griddataset = GridDataset(n, num_samples)
+gridloader = DataLoader(griddataset, batch_size=8, shuffle=True)
+
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    for batch_idx, batch_grids in enumerate(gridloader):
+        batch_actions = []
+        batch_log_probs = []
+        batch_rew = []
+        ## rollout
+        for i in  range(batch_grids.shape[0]):
+            grid = batch_grids[0]
+            grid_tensor = torch.tensor(grid, dtype=torch.float)
+            grid_tensor = grid_tensor.view(-1, 5, 5)
+            mapper = ConvNet(grid_size=n)
+            dist_map = mapper(grid_tensor)
+            dist_map_numpy = dist_map.detach().numpy()
+            actions = []
+            log_probs = []
+            for i in range(n-1):
+                action = random.choices(possible_actions, dist_map_numpy[:, i])[0]
+                log_prob = np.log(dist_map_numpy[:, i][action])
+                actions.append(action)
+                log_probs.append(log_prob)
+            log_probs = np.sum(log_probs)
+            actions = np.array(actions)
+            y = path_planner.action2point(actions)
+            p = np.column_stack((x, y))
+            path = path_planner.construct_sp(p)
+            cost = path_planner.calculate_cost(path, target, grid)
+            batch_rew.append(cost)
+            batch_actions.append(actions)
+            batch_log_probs.append(log_probs)
+        batch_rew = torch.tensor(batch_rew, dtype=torch.float32)
+        batch_actions = torch.tensor(batch_actions, dtype=torch.float32)
+        batch_log_probs = torch.tensor(batch_log_probs, dtype=torch.float32)
+        print(f'Batch {batch_idx} done')
 
 # If you want to use DataLoader for batching, etc.
-dataloader = DataLoader(grids, batch_size=8, shuffle=True)
 # plt.imshow(grid, cmap='gray')
 
 ## network output
@@ -31,7 +68,7 @@ print('dist_map = ', dist_map)
 # plt.imshow(dist_map, cmap='gray')
 
 ## bspline
-possible_actions = np.arange(0, 5)
+
 actions = []
 for i in range(n-1):
     action = random.choices(possible_actions, dist_map[:, i])[0]
