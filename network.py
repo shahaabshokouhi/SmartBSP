6897bbcba14bb675c8d74b705b65a05f3dcd5bb4
Shahab Mac <shokohishahab@gmail.com>
2024-02-26 10:56:59 -0500

    Larger value estimation network (works better)

--- network.py
+++ network.py
@@ -35,14 +35,23 @@ class ConvVal(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.flattened_size = grid_size * grid_size
-        self.fc = nn.Linear(self.flattened_size, 1)
+        self.fc1 = nn.Linear(self.flattened_size, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 1)
     def forward(self, x):
         x = self.conv1(x)
         x = self.relu(x)
         x = self.conv2(x)
         x = self.relu(x)
         x = x.view(-1, self.flattened_size)
-        x = self.fc(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.fc4(x)
         return x
 
 
